d8abfb7e240d8c408bdd44708b202cf5efb4289c
Kleinsasser <mkleinsa@UMICH.EDU>
2019-11-05 14:27:14 -0500

    added data sets

---
+++ tests/testthat/test_penRank_path.R
@@ -0,0 +1,24 @@
+
+context("Match penRank_path results")
+
+# split this into the important parts of the output
+# and test for each built in use case
+test_that("Unpenalized penRank_path results match for case 23", {
+  
+  # insert single run up here?
+  
+  expect_equal()
+  expect_equal()
+  expect_equal()
+  expect_equal()
+  
+})
+
+test_that("Penalized penRank_path results match for case 23", {
+  
+  expect_equal()
+  expect_equal()
+  expect_equal()
+  expect_equal()
+  
+})
