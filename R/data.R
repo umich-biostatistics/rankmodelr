0f4242271cb62480e6d61888e41699bb13cf56bf
Kleinsasser <mkleinsa@UMICH.EDU>
2019-11-05 15:05:11 -0500

    edited internal data

---
+++ R/data.R
@@ -0,0 +1,30 @@
+
+#' Add title
+#'
+#' Add description
+#'
+#'
+#'
+#'
+#' @export
+"case83_ordered"
+
+#' Add title
+#'
+#' Add description
+#'
+#'
+#'
+#'
+#' @export
+"case23_ordered"
+
+#' Add title
+#'
+#' Add description
+#'
+#'
+#'
+#'
+#' @export
+"case111_ordered"
